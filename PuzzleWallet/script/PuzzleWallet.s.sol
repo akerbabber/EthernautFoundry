6a0873fe3c47f8cbe3a83be8d0244932f139e2c0
Maurizio Murru <maurizio.murru93@gmail.com>
2023-07-02 11:09:54 +0200

    PuzzleWallet challenge done.

--- PuzzleWallet/script/PuzzleWallet.s.sol
+++ PuzzleWallet/script/PuzzleWallet.s.sol
@@ -1,13 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-import "forge-std/Test.sol";
+import "forge-std/Script.sol";
 import "../src/PuzzleWallet.sol";
 
-contract PuzzleWalletTest is Test {
+contract PuzzleWalletScript is Script {
 // research on payable multicalls https://github.com/Uniswap/v3-periphery/issues/52
     
-    function test() public {
+    function run() public {
         address payable proxyAddress = payable(0x21C5025330E3aB520538Ad83e2fb7e57425557A4);
         address EOA = vm.rememberKey(vm.envUint("PRIVATE_KEY"));
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
